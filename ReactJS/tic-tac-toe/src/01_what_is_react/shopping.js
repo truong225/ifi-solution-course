7fe86f842c7840f9c3e7560d11e4a39d3d3e0546
truong225 <truongmero225@gmail.com>
2018-04-20 10:32:03 +0700

    + first tutorial: tic-tac-toe game

---
+++ ReactJS/tic-tac-toe/src/01_what_is_react/shopping.js
@@ -0,0 +1,17 @@
+class ShoppingList extends React.Component{
+    /**
+     * This component tell React what wanted to be render
+     */
+    render(){
+        return (
+            <div className="shopping-list">
+                <h1>Danh sach mua sam for{this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        )
+    };
+}
